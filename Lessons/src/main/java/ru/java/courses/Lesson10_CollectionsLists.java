8c70b0a6e0c519efeafd9210df03cd75e6ce2b56
Neznaev Semen <neznaev@reksoft.ru>
2022-01-31 10:39:05 +0300

    Lesson 10 homework fix

--- Lessons/src/main/java/ru/java/courses/Lesson10_CollectionsLists.java
+++ Lessons/src/main/java/ru/java/courses/Lesson10_CollectionsLists.java
@@ -21,11 +21,22 @@ public class Lesson10_CollectionsLists {
     public static List<String> task1(List<Integer> source) {
         List<String> result = new ArrayList<>(source.size());
         Collections.sort(source);
-        for (int item : source){
+        int i = 0;
+        while (i < source.size()){
+            result.add(String.valueOf(source.get(i)));
+            i = source.lastIndexOf(source.get(i))==i ? i+1 : source.lastIndexOf(source.get(i)) + 1;
+        }
+        /*result.add(String.valueOf(source.get(0)));
+        for(int i = 1; i < source.size(); i++){
+            if(!source.get(i).equals(source.get(i-1))){
+                result.add(String.valueOf(source.get(i)));
+            }
+        }*/
+        /*for (int item : source){
             if (result.lastIndexOf(String.valueOf(item)) == -1){
                 result.add(String.valueOf(item));
             }
-        }
+        }*/
         return result;
     }
 
